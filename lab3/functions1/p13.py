0dd27986cb63a8f289863f2550c344d2864dca1a
Aiba1 <ai_saduakas@kbtu.kz>
2024-02-10 17:24:32 +0600

    lab3

--- lab3/functions1/p13.py
+++ lab3/functions1/p13.py
@@ -1,23 +1,14 @@
 import random
-def inter(num,num1):
-    i=0
-    x=random.randint(num,num1)
-    print(x,"\n")
-    m=input()
-    if m=="Your guess is too low. Take a guess.":
-        inter(x,20)
-        i+=1
-    elif m=="Your guess is too high. Take a guess.":
-        inter(1,x)
-        i+=1
-    elif m=="Good job, KBTU! You guessed my number":
-        return
-        
-
-    
-a=input()
-if a=="Hello! What is your name?":
-    print("KBTU\n")
-b=input()
-if b=="Well, KBTU, I am thinking of a number between 1 and 20. Take a guess.":
-    inter(1,20)
+def ran(x,a,counter):
+    if(a == x):
+        return counter
+    elif(a<x):
+        a = int(input("Your guess is too low. Take a guess."))
+        return ran(x,a,counter+1)
+    elif(a>x):
+        a = int(input("Your guess is too high. Take a guess."))
+        return ran(x,a,counter+1)
+x = random.randint(1,20)
+name = input('Hello! What is your name?')
+a = int(input('Well, '+name+', I am thinking of a number between 1 and 20.Take a guess.'))
+print('Good job, '+name+'! You guessed my number in '+str(ran(x,a,1))+' guesses!')
